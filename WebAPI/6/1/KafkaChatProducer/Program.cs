0425c628ebd1be06b83d32e0e0d417727e333664
soumD1234prof-dot <desoumitra1234.prof@gmail.com>
2026-07-19 15:39:25 +0530

    Add files via upload

---
+++ WebAPI/6/1/KafkaChatProducer/Program.cs
@@ -0,0 +1,16 @@
+﻿using Confluent.Kafka;
+
+var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+Console.WriteLine("Kafka Chat Producer - type a message and press Enter (type 'exit' to quit)");
+
+while (true)
+{
+    string? message = Console.ReadLine();
+    if (message == "exit") break;
+
+    var result = await producer.ProduceAsync("chat-topic", new Message<Null, string> { Value = message });
+    Console.WriteLine($"Delivered to: {result.TopicPartitionOffset}");
+}
