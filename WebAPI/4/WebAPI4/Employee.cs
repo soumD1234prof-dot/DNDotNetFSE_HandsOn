95e654257b01134c6d7a80e72816675bcaf500b7
soumD1234prof-dot <desoumitra1234.prof@gmail.com>
2026-07-19 15:31:16 +0530

    Add files via upload

---
+++ WebAPI/4/WebAPI4/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI4
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Salary { get; set; }
+        public bool Permanent { get; set; }
+    }
+}
---
+++ WebAPI/4/WebAPI4/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Employee CRUD Demo",
+        Version = "v1"
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Employee CRUD Demo");
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
