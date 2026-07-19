880a601f5a3451e98d8967b5e948c8787abda242
soumD1234prof-dot <desoumitra1234.prof@gmail.com>
2026-07-19 20:52:36 +0530

    Add files via upload

---
+++ WebAPI/6/2/KafkaChatConsumer1/KafkaChatConsumer1/Program.cs
@@ -0,0 +1,19 @@
+﻿using Confluent.Kafka;
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092",
+    GroupId = "chat-consumer-group-1",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+consumer.Subscribe("chat-topic");
+
+Console.WriteLine("Consumer 1 listening for chat messages...");
+
+while (true)
+{
+    var result = consumer.Consume();
+    Console.WriteLine($"Received: {result.Message.Value}");
+}
