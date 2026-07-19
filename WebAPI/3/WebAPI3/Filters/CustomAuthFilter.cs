614435d6f145f6bd6c2a857d07faf2944be25567
soumD1234prof-dot <desoumitra1234.prof@gmail.com>
2026-07-19 15:27:45 +0530

    Add files via upload

---
+++ WebAPI/3/WebAPI3/Filters/CustomAuthFilter.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebAPI3.Filters
+{
+    public class CustomAuthFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            // Skip this filter entirely if the action allows anonymous access
+            var allowAnonymous = context.ActionDescriptor.EndpointMetadata
+                .Any(em => em.GetType() == typeof(AllowAnonymousAttribute));
+
+            if (allowAnonymous)
+            {
+                base.OnActionExecuting(context);
+                return;
+            }
+
+            if (!context.HttpContext.Request.Headers.ContainsKey("Authorization"))
+            {
+                context.Result = new BadRequestObjectResult("Invalid request - No Auth token");
+                return;
+            }
+
+            var authHeader = context.HttpContext.Request.Headers["Authorization"].ToString();
+            if (!authHeader.Contains("Bearer"))
+            {
+                context.Result = new BadRequestObjectResult("Invalid request - Token present but Bearer unavailable");
+                return;
+            }
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
---
+++ WebAPI/3/WebAPI3/Filters/CustomExceptionFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebAPI3.Filters
+{
+    public class CustomExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            var exceptionDetail = context.Exception.ToString();
+
+            //exception detail
+            var logPath = Path.Combine(AppContext.BaseDirectory, "error_log.txt");
+            File.AppendAllText(logPath, $"{DateTime.Now}: {exceptionDetail}{Environment.NewLine}{Environment.NewLine}");
+
+            context.Result = new ObjectResult(new { message = "An unexpected error occurred!!!", detail = context.Exception.Message })
+            {
+                StatusCode = StatusCodes.Status500InternalServerError
+            };
+
+            context.ExceptionHandled = true;
+        }
+    }
+}
