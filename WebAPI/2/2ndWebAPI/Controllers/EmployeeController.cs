912a9dd33780296fdf5a916d25612d41b5ecacd9
soumD1234prof-dot <desoumitra1234.prof@gmail.com>
2026-07-19 15:21:59 +0530

    Add files via upload

---
+++ WebAPI/2/2ndWebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using _2ndWebAPI;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _2ndWebAPI.Controllers
+{
+    [Route("api/Emp")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private static readonly List<Employee> Employees = new()
+        {
+            new Employee { Id = 1, Name = "Asha Rao", Department = "Engineering" },
+            new Employee { Id = 2, Name = "Vikram Shah", Department = "Sales" }
+        };
+
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            return Employees;
+        }
+    }
+}
---
+++ WebAPI/2/2ndWebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _2ndWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value) { }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value) { }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id) { }
+    }
+}
