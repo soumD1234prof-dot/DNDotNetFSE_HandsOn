4f40567d400e8fc75291e836fae628e82e0f1e38
soumD1234prof-dot <desoumitra1234.prof@gmail.com>
2026-07-21 22:12:17 +0530

    Add files via upload

---
+++ React/14/employeesapp/src/EmployeeCard.js
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import ThemeContext from './ThemeContext';
+import Styles from './EmployeeCard.module.css';
+
+function EmployeeCard(props) {
+    const theme = useContext(ThemeContext);
+    return (
+        <div className={Styles.Card}>
+            <h3>{props.employee.name}</h3>
+            <p>{props.employee.email}</p>
+            <p>{props.employee.phone}</p>
+            <p>
+                <a href="#" className={theme}>Edit</a> 
+                <a href="#" className={theme}>Delete</a> 
+            </p>
+        </div>
+    );
+}
+export default EmployeeCard;
+
